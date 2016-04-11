4b3e39367b307717505c2afc6ae14f173485f022
Costin Galan <cgalan@cloudbasesolutions.com>
2016-04-11 15:38:23 +0300

    Second draft of the script.

--- generate_script.ps1
+++ generate_script.ps1
@@ -1,17 +1,23 @@
-Remove-Module WinImageBuilder.psm1
+if (Get-Module WinImageBuilder) {
+    Remove-Module WinImageBuilder
+}
 Import-Module .\WinImageBuilder.psm1
 
 #This is the content of your Windows ISO
-$wimFilePath = "D:\sources\install.wim"
+$isoPath = Join-Date -ChildPath $env:isoName -Path $env:folder
+$driveLetter = (Mount-DiskImage $isoPath -PassThru | Get-Volume).DriveLetter 
+$wimFilePath = "${driveLetter}:\sources\install.wim"
 
 # Check what images are supported in this Windows ISO
 $images = Get-WimFileImagesInfo -WimFilePath $wimFilePath
 
-$MaaSImagePath = "$buildArea\"
+$imageName = (Get-Date).ToString('ddMMyyy') + '-' + $env:BUILD_NUMBER + '-dd'
+
+$targetPath = Join-Path -Path "C:\generate_windows_images\generated_images" -ChildPath "$imageName"
 
 # Choosing the standard edition
 $image = $images[1]
 
 New-MaaSImage -WimFilePath $wimFilePath -ImageName $image.ImageName`
--MaaSImagePath C:\images\win2012hvr2-dd -SizeBytes 16GB -Memory 4GB `
--CpuCores 2 -DiskLayout BIOS -RunSysprep
+-MaaSImagePath $targetPath -SizeBytes 45GB -Memory 8GB `
+-CpuCores 4 -DiskLayout BIOS -RunSysprep -
