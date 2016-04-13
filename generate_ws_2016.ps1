f0a198eb9ba602bb535857bffd5e66d5b4cad62f
Costin Galan <cgalan@cloudbasesolutions.com>
2016-04-13 19:11:50 +0300

    Revert "Changed the loggin capability"
    This reverts commit d535f2b03a3c9649f981e095a0d23283a788ccb8.

--- generate_ws_2016.ps1
+++ generate_ws_2016.ps1
@@ -5,10 +5,13 @@
 
 $baseDir = "C:\generate_windows_images"
 $buildArea = Join-Path -Path "$baseDir" -ChildPath "build_area"
+$logDir = Join-Path -Path "$buildArea" -ChildPath "logs"
 $woitDir = Join-Path -Path "$buildArea" -ChildPath "woit-$env:BUILD_NUMBER"
 $scriptDir = Join-Path -Path "$buildArea" -ChildPath "ws2016-jenkins-$env:BUILD_NUMBER"
 $isoDir = Join-Path -Path "$baseDir" -ChildPath "generated_images" 
 $isoPath = Join-Path -ChildPath "$env:isoName" -Path "$env:folder"
+$logName = (Get-Date).ToString('ddMMyyy') + '-' + "$env:BUILD_NUMBER" + '.txt'
+$logPath = Join-Path -Path "$logDir" -ChildPath "$logName"
 $imageName = (Get-Date).ToString('ddMMyyy') + '-' + "$env:BUILD_NUMBER" + '-dd'
 $targetPath = Join-Path -Path "$isoDir" -ChildPath "$imageName"
 $virtPath = Join-Path -Path "$baseDir" -ChildPath "optional_images\virtio-win-0.1.102.iso"
@@ -29,7 +32,7 @@ try {
     }
     git clone https://github.com/costingalan/ws2016-jenkins "ws2016-jenkins-$env:BUILD_NUMBER"
     pushd "$scriptDir"
-    .\generate_script.ps1 
+    .\generate_script.ps1 | Tee-Object -FilePath "$logPath"
     popd
     
     popd
