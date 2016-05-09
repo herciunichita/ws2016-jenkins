b44dc28a3eeb829d429b60344be58c0ead3856fe
Costin Galan <cgalan@cloudbasesolutions.com>
2016-05-09 15:10:07 +0300

    Testing the disable_swap from sync-master-experimental
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

--- ader_generate_ws_2016.ps1
+++ ader_generate_ws_2016.ps1
@@ -50,9 +50,9 @@ try {
     if (Test-Path "$woitDir") {
         Remove-Item -Recurse -Force "$woitDir"
     }
-    git clone -b disable-swap https://github.com/ader1990/windows-openstack-imaging-tools "ader-woit-$env:BUILD_NUMBER" 
+    git clone -b disable_swap https://github.com/costingalan/windows-openstack-imaging-tools "ader-woit-$env:BUILD_NUMBER" 
     pushd "$woitDir"
-    git checkout disable-swap
+    git checkout disable_swap
     git submodule update --init #for the curtin and update modules
     popd
     ls
